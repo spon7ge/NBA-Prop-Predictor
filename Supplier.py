693cd44c35d6223f6e5d8079aafa9e15b78f205c
Alex Gonzalez <alexg@Alex-2318.local>
2025-06-16 19:01:48 -0700

    updated the time and space on functions that took a long time

--- Supplier.py
+++ Supplier.py
@@ -5,8 +5,8 @@
 
 class Supplier():
     def __init__(self):
-        self.api_key = ""
-        self.directory = ""
+        self.api_key = '2a1f2c073b529f516df87b0e8a459946'
+        self.directory = '/Users/alexg/Downloads/projections.json'
     
     def getKey(self):
         return self.api_key
