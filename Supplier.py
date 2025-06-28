4720362fabf2bc789177f81862790d4c417d9e74
Alex Gonzalez <alexg@Alex-2318.local>
2025-06-28 06:23:26 -0700

    removed key

--- Supplier.py
+++ Supplier.py
@@ -1,12 +1,12 @@
-# from dotenv import load_dotenv
-# import os
+from dotenv import load_dotenv
+import os
 
-# load_dotenv()
+load_dotenv()
 
 class Supplier():
     def __init__(self):
-        self.api_key = "3b9be537d24e5c74795a41ce44314afc"
-        self.directory = '/Users/alexg/Downloads/projections.json'
+        self.api_key = os.environ.get('ODDS_API_KEY')
+        self.directory = os.environ.get('PROJECTIONS_DIRECTORY')
     
     def getKey(self):
         return self.api_key
