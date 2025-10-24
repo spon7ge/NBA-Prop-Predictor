eccb745a18b5c134e8925a54a8b71b30197ba9a3
Alex Gonzalez <alexg@Alex-2318.local>
2025-10-24 15:02:25 -0700

    final changes

--- PropFinder.py
+++ PropFinder.py
@@ -2,7 +2,7 @@ from NBAPropFinder.NBAPropFinder import NBAPropFinder
 from WNBAPropFinder.WNBAPropFinder import WNBAPropFinder
 from NBAPropFinder.TeamLines import fetchTeamLines
 from datetime import datetime
-api_key = '' 
+api_key = '39f23451ab7ecb6a361792def17f4f78' 
 
 # recomend using one of these at a time
 nba_props = NBAPropFinder(region='us_dfs')
