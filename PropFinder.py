1aaa28539267e4edb158cab74b32ebceae11fa0a
Alex Gonzalez <alexg@Alex-2318.local>
2025-10-25 15:55:24 -0700

    removed older prop lines

--- PropFinder.py
+++ PropFinder.py
@@ -5,6 +5,6 @@ from datetime import datetime
 api_key = '39f23451ab7ecb6a361792def17f4f78' 
 
 # recomend using one of these at a time
-nba_props = NBAPropFinder(region='us_dfs')
+nba_props = NBAPropFinder(region='us')
 # nba_team_lines = fetchTeamLines(api_key, date=datetime.now().strftime("%Y%m%d"))
 # wnba_props = WNBAPropFinder(region='us')
